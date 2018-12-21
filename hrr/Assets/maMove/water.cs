0ba6f96c6105e7d55bf37339944e6cace466f3f8
Roberto Schenk <roberto.schenk@voxstudio.com.br>
2018-12-21 14:02:28 -0200

    Revert "eeeeeeee"
    This reverts commit 79ba7a3419459befe907bcbc71e4d5e19483c7c8.

---
+++ hrr/Assets/maMove/water.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public interface SOLIDBois
+{
+	void AAAA();
+	
+}
+public class RERERe : SOLIDBois
+{
+    public void AAAA()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public class water : MonoBehaviour {
+
+	// Use this for initializationgfdgdfg
+	dfh
+	rtyy
+	rtye
+	waterrew
+	[Fact]
+	public void sd
+	g
+	fgh
+	fcf
+	vb
+	fg
+	her
+	t
+	wde
+	fs
+	dcg
+	v
+	hrt
+	y
+	
+	g
+	df
+	g
+	weq
+	t
+	hgt
+	
+	d
+	()
+	{
+	//Given
+	
+	//When
+	
+	//Then
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SOLIDBois __thisdude = new RERERe();
+		__thisdude.AAAA();
+
+		//Logic for the future
+		/*
+		fdsopfewprkwepfr
+		gf
+		dg
+		df
+		ger
+		t
+		er
+		t
+		ytrpopgifp´gdfhfg
+		j
+		tr
+		y
+		rty
+		rt
+		
+		
+		
+		 */
+	}
+}
