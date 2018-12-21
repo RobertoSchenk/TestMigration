7aca9814b9a773fad1975a5d859324799b884d66
Roberto Schenk <roberto.schenk@voxstudio.com.br>
2018-12-21 11:32:14 -0200

    project start

---
+++ hrr/Assets/maMove/lalalalala.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lalalalala : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("AAAAA");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print("Bug que vai zoar o bang todo");
+	}
+}
---
+++ hrr/Assets/maMove/water.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public interface SOLIDBois
+{
+	void AAAA();
+	
+}
+public class RERERe : SOLIDBois
+{
+    public void AAAA()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public class water : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SOLIDBois __thisdude = new RERERe();
+		__thisdude.AAAA();
+	}
+}
